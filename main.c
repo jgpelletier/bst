6e420b6bd9b1f2bf78e8c60d99f5f17407a4c34e
Joshua Pelletier <jgpelletier@gmail.com>
2014-09-23 17:20:07 +0000

    an int is assigned to a node in the node_pool

--- main.c
+++ main.c
@@ -5,17 +5,19 @@
 
 int main ()
 {
-    static node_t nodes[15];
-    int x = 0;
+    node_t nodes[15];
+    int x = 1111;
 
     np_pool_t node_pool;
+    node_t node;
 
     np_initialize(&node_pool, sizeof(node_t), nodes, sizeof(nodes));
 
-    // vvv this doesn't work.
-    node_t* node = (node_t*) new_node(&x, np_allocate(&node_pool));
+    (node_t*) new_node(&x, np_allocate(&node_pool));
 
-    //printf("node value %d\n", nodes->data);
+    node = nodes[0];
+
+    printf("node value %d\n", *node.data);
 
     return 0;
 }
