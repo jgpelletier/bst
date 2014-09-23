fa67922daca424474c639c3b8df1e4c59a7cf382
Joshua Pelletier <jgpelletier@gmail.com>
2014-09-23 21:55:10 +0000

    implementing a linked-list

--- main.c
+++ main.c
@@ -5,19 +5,28 @@
 
 int main ()
 {
-    static node_t nodes[15]; // 2219 seg fault with 15 nodes. added another node to make it 16.
+    static node_t nodes[16]; // 2219 seg fault with 15 nodes. added another node to make it 16.
                              // is this bc it points to the beginning of the 16th node?
     int i;
-
+    node_t *head, *node, *temp;
     np_pool_t node_pool;
-
     np_initialize(&node_pool, sizeof(node_t), nodes, sizeof(nodes));
-
     static int arr[15] = { 9, 4, 8, 7, 0, 10, 5, 14, 1, 11, 24, 19, 18, 34, 17 };
 
     for ( i = 0; i < 15; ++i) {
         (node_t*) new_node(&arr[i], np_allocate(&node_pool));
-         printf("i value %d and node value %d\n", i, *nodes[i].data);
+
+        if (head == NULL) {
+            head = &nodes[i];
+        } else {
+            node = head;
+            while (node->right != NULL) {
+               node = node->right;
+            }
+            node->right = &nodes[i];
+        }
+
+         //printf("i value %d and node value %d\n", i, *nodes[i].data);
 
     }
 
